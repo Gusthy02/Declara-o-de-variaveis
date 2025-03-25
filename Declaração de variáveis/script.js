f6d5d788cf25a32427089d6d222f62585776c9a8
Gustavo Thyerris <gusthyerris@gmail.com>
2025-03-25 15:05:19 -0300

    Create JS files

---
+++ Declaração de variáveis/script.js
@@ -0,0 +1,16 @@
+const melhorGuitarrista = "Jimi Hendrix";
+
+let a = 1;
+    b = 2;
+    c = 3;
+
+let salarioMinimo = 1518;
+
+/* As constantes possuem valores fixos, enquanto as vpariáveis (let)
+                 pdoer ter seus valores alterados. */
+
+/* O var não é recomendado, pois o var não respeita escopos de bloco,
+                        apenas de funções. */
+
+/* é bem melhor utilizar "null" quando quiserrepresentar um valor nulo,
+        evitando assim ambiguidade e inconsistencias no código. */
